e175ef990f8ca724a625625c17822fdd8392ccbd
dorvak <dorvakd9@gmail.com>
2014-04-01 17:28:50 +0400

    Админка. Управление пользователями

---
+++ admin/application/controllers/users_controller.php
@@ -0,0 +1,202 @@
+<?php
+	/*
+		TODO: права на вложенные страницы
+	*/
+	
+	class users_controller extends application_controller {
+		
+		//Список пользователей и ролей
+		public function index() {
+			if (!$this->CheckAccess(1)) $this->AccessError();
+			
+			$role_id = $this->session->get('role_id');
+			if ($role_id > 0) $role['self'] = 1;
+			
+			//Список пользователей
+			$user_id = $this->session->get('admin');
+			$role['users'] = $this->users->getUsers();
+			if (!empty($role['users'])) {
+				foreach ($role['users'] as &$v) {
+					if ($v['role_id'] == 0) $v['role'] = '&nbsp;нет роли';
+					else $v['role'] = '<a href="/admin/role/role/'.$v['role_id'].'/" style="margin-left: 3px;">'.$v['title'].'</a>';
+					
+					if ($v['pid'] == 0) $v['creator'] = '&nbsp;Главный администратор';
+					else $v['creator'] = '<a href="/admin/role/user/'.$v['pid'].'/" style="margin-left: 3px;">'.$v['creator'].'</a>';
+				}
+			} else $role['message'] = '<div style="margin:5px 0 5px 14px;">Пользователей нет</div>';
+									
+			$this->html->tpl_vars['content_path'] = $this->html->render('users/view.html', $role);
+		}
+		
+		//Вызов ошибки прав
+		public function AccessError() {
+			$this->session->set('alert','У вас нет прав на эту операцию');
+			$this->url->redirect('::referer');
+			die();
+		}
+		
+		//Проверка можно ли сделать действие
+		public function CheckAccess($action, $param1 = false) {
+			$user_id = $this->session->get('admin');
+			if ($user_id === 0) return true;	//Если главный - всё можно
+			
+			$rights = $this->session->get('rights');
+			if (empty($rights)) {				//Если вообще нет прав, то выходим
+				return false;
+			}
+			
+			if (empty($param1)) {				//Если параметр не указан - значит он не имеет значения и можно проверить только действие
+				return (isset($rights[$action]));
+			} else {							//Иначе проверяем действие с параметром
+				return (isset($rights[$action][$param1]));
+			}
+		}
+		
+		//Добавление пользователя
+		public function add_user() {
+			if (!$this->CheckAccess(1)) $this->AccessError();
+			
+			if (!empty($_POST)) {
+				$login = empty($_POST['login']) ? '' : $_POST['login'];
+				$pass = empty($_POST['pass']) ? '' : $_POST['pass'];
+				$pass2 = empty($_POST['pass2']) ? '' : $_POST['pass2'];
+				
+				$name = empty($_POST['name']) ? '' : strip_tags($_POST['name']);
+				$lastname = empty($_POST['lastname']) ? '' : strip_tags($_POST['lastname']);
+				$middlename = empty($_POST['middlename']) ? '' : strip_tags($_POST['middlename']);
+				
+				if ( $pass != $pass2 ) {
+					$this->session->set('alert', 'Пароли не совпадают!');
+					$this->url->redirect('::referer','/admin/role/');
+				}
+				
+				$login = mb_strtolower(strip_tags($login), 'UTF-8');
+				$pass = md5( 'egi'.mb_strtolower($pass, 'UTF-8').'pet' );
+											
+				
+				$data = array(
+					'login'			=>	$login,				
+					'name'			=>	$name,				
+					'lastname'		=>	$lastname,				
+					'middlename'	=>	$middlename,				
+					'pass'			=>	$pass,					
+					'email' 		=> 	trim(mb_strtolower($_POST['email'],'UTF-8')),
+				);
+				
+				if ($this->users->HasLogin($login) == false) {
+					$id = $this->db->insert('users', $data);
+					if ($id) {	
+						$this->url->redirect('/admin/users/');
+					} else $this->session->set('alert', 'Ошибка добавления');
+				} else $this->session->set('alert', 'Логин занят!');
+				
+				$this->url->redirect('::referer','/admin/users/');
+			}
+			$role_id = $this->session->get('role_id');
+			$user['roles'] = $this->role->getSubRoles($role_id);
+			
+			$this->html->tpl_vars['content_path'] = $this->html->render('users/add_user.html', $user);
+		}
+		
+		
+		//Редактирование пользователя
+		public function user($id = false) {
+			if (!$this->CheckAccess(1)) $this->AccessError();
+			$user_id = $this->session->get('admin');
+			$users = $this->users->getSubUsers($user_id, true);
+			$id = empty($_POST['id']) ? (int)$id : (int)$_POST['id'];
+					
+			if (!empty($_POST)) {
+				if (!$id) {
+					$this->session->set('alert', 'Ошибка добавления');
+					$this->url->redirect('::referer','/admin/users/');
+				}
+				
+				$login = empty($_POST['login']) ? '' : $_POST['login'];
+				$email = empty($_POST['login']) ? '' : trim(mb_strtolower($_POST['email'],'UTF-8'));
+				$pass = empty($_POST['pass']) ? '' : $_POST['pass'];
+				$pass2 = empty($_POST['pass2']) ? '' : $_POST['pass2'];
+				
+				$login = mb_strtolower(strip_tags($login), 'UTF-8');
+				
+				$name = empty($_POST['name']) ? '' : strip_tags($_POST['name']);
+				$lastname = empty($_POST['lastname']) ? '' : strip_tags($_POST['lastname']);
+				$middlename = empty($_POST['middlename']) ? '' : strip_tags($_POST['middlename']);				
+				
+				$data = array(
+					'login'			=>	$login,
+					'name'			=>	$name,
+					'lastname'		=>	$lastname,
+					'middlename'	=>	$middlename,
+					'email'			=>	$email,
+				);
+				
+				if (!empty($pass)) {
+					if ($pass != $pass2) {
+						$this->session->set('alert', 'Пароли не совпадают!');
+						$this->url->redirect('::referer','/admin/users/');
+					} else {
+						$pass = md5( 'egi'.mb_strtolower($pass, 'UTF-8').'pet' );
+						$data['pass'] = $pass;
+					}
+				}
+				
+				if ($this->users->HasLoginNew($id, $login) == false) {
+					$this->db->update('users', $data, $id);
+				} else $this->session->set('alert', 'Логин занят!');
+				
+				$this->session->set('alert', 'Изменения сохранены');
+				$this->url->redirect('/admin/users/');
+			}
+			$user = $this->users->getUser($id);				
+			
+			$this->html->tpl_vars['content_path'] = $this->html->render('users/user.html', $user);
+		}
+		
+		
+		//Ajax проверка существования логина
+		public function CheckUserLogin() {
+			$login = empty($_POST['login']) ? '' : $_POST['login'];
+			if ($this->users->HasLogin($login)) die(true);
+			die(false);
+		}
+		
+		//Ajax проверка существования логина при изменении
+		public function CheckUserLoginNew() {
+			$id = empty($_POST['id']) ? 0 : (int)$_POST['id'];
+			$login = empty($_POST['login']) ? '' : $_POST['login'];
+			if ($this->users->HasLoginNew($id, $login)) die(true);
+			die(false);
+		}
+		
+		//Ajax проверка существования логина при изменении
+		public function CheckRoleTitle() {
+			$title = empty($_POST['title']) ? '' : $_POST['title'];
+			if ($this->users->HasRoleTitle($title)) die(true);
+			die(false);
+		}
+		
+		//Ajax проверка существования логина при изменении
+		public function CheckRoleTitleNew() {
+			$id = empty($_POST['id']) ? 0 : (int)$_POST['id'];
+			$title = empty($_POST['title']) ? '' : $_POST['title'];
+			if ($this->users->HasRoleTitleNew($id, $title)) die(true);
+			die(false);
+		}
+		
+		//Удаление пользователя
+		public function DeleteUser($id) {
+			if (!$this->CheckAccess(1)) $this->AccessError();
+			
+			$id = empty($_POST['id']) ? (int)$id : (int)$_POST['id'];
+						
+			$this->db->delete('users', $id);			
+			
+			$this->url->redirect('::referer', '/admin/users/');
+		}
+		
+		//==========================================================
+		
+		
+	}
+?>
---
+++ admin/application/models/users.php
@@ -0,0 +1,186 @@
+<?php
+	class users extends app_model {
+		
+		
+		public function getUsers() {
+			$sql = 'SELECT u.* 
+					FROM users u 
+					ORDER BY u.id DESC';
+			return $this->db->get_all($sql);
+		}	
+
+		public function getUser($id) {
+			$sql = 'SELECT u.* 
+					FROM users u 
+					WHERE u.id = '.$id;
+			return $this->db->get_row($sql);
+		}
+		
+		//¬ыборка пользовател€ по логину
+		public function getUserByLogin($login) {
+			$sql = 'SELECT au.*, ar.title FROM auth_user au
+					LEFT JOIN auth_role ar ON au.role_id = ar.id
+					WHERE au.login = "'.$login.'" LIMIT 1';
+			return $this->db->get_row($sql);
+		}
+		
+		
+		//¬ыборка роли и прав роли
+		public function getRole($role_id) {
+			$sql = 'SELECT * FROM auth_role
+					WHERE id = '.(int)$role_id;
+			$res = $this->db->get_row($sql);
+			return $res;
+		}
+		
+		//¬ыборка прав роли (дл€ запоминани€ при авторизации)
+		public function getRoleRights($role_id) {
+			$sql = 'SELECT ar.action_id, ar.param1 FROM auth_rights ar
+					LEFT JOIN auth_action aa ON aa.id = ar.action_id
+					WHERE ar.role_id = '.$role_id;
+			$tmp = $this->db->get_all($sql);
+			$res = array();
+			if (!empty($tmp)) {
+				foreach ($tmp as &$v) {
+					if ( !isset($v['param1']) ) $res[$v['action_id']] = 1;
+					else $res[$v['action_id']][$v['param1']] = 1;
+				}
+			}
+			return $res;
+		}
+		
+		//======================
+		
+		//ѕроверка существовани€ логина
+		public function HasLogin($login) {
+			$sql = 'SELECT 1 FROM users
+					WHERE login = '.$this->db->escape($login).' LIMIT 1';
+			return $this->db->get_one($sql);
+		}		
+					
+		//ѕроверка существовани€ логина кроме того, чей id указан
+		public function HasLoginNew($id, $login) {
+			$sql = 'SELECT 1 FROM users
+					WHERE login = '.$this->db->escape($login).' AND id<>'.(int)$id.' LIMIT 1';
+			return $this->db->get_one($sql);
+		}
+		
+		//ѕроверка существовани€ названи€ роли
+		public function HasRoleTitle($title) {
+			$sql = 'SELECT 1 FROM auth_role
+					WHERE title = '.$this->db->escape($title).' LIMIT 1';
+			return $this->db->get_one($sql);
+		}
+		
+		//ѕроверка существовани€ названи€ роли кроме той, чей id указан
+		public function HasRoleTitleNew($id, $title) {
+			$sql = 'SELECT 1 FROM auth_role
+					WHERE title = '.$this->db->escape($title).' AND id<>'.(int)$id.' LIMIT 1';
+			return $this->db->get_one($sql);
+		}
+		
+		//¬ыборка количества пользователей с каждой ролью
+		public function getRoleUserCount() {
+			$sql = 'SELECT ar.*, tmp.count FROM auth_role ar
+							LEFT JOIN
+							(SELECT au.role_id, COUNT(*) count FROM auth_user au
+							GROUP BY au.role_id) AS tmp
+							ON ar.id=tmp.role_id';
+			$tmp = $this->db->get_all($sql);
+			$res = array();
+			if (!empty($tmp))
+				foreach ($tmp as $v) {
+					$res[$v['id']] = (int)$v['count'];
+				}
+			return $res;
+		}
+		
+		//=============================
+		
+		//¬ыборка дочерних пользователей
+		public function getSubUsers($pid, $only_ids = false) {
+			$res = array();
+			$this->getSubUsersLevel($pid, $res, $only_ids);
+			return $res;
+		}
+		
+		private function getSubUsersLevel($pid, &$res, $only_ids) {
+			if ($only_ids) {
+				$sql = 'SELECT au.id FROM auth_user au
+						LEFT JOIN auth_role ar ON au.role_id = ar.id
+						LEFT JOIN auth_user au2 ON au2.id = au.pid
+						WHERE au.pid = '.(int)$pid.'
+						ORDER BY au.id';
+				$tmp = $this->db->get_all_one($sql);
+			} else {
+				$sql = 'SELECT au.*, au2.login creator, ar.title FROM auth_user au
+						LEFT JOIN auth_role ar ON au.role_id = ar.id
+						LEFT JOIN auth_user au2 ON au2.id = au.pid
+						WHERE au.pid = '.(int)$pid.'
+						ORDER BY au.id';
+				$tmp = $this->db->get_all($sql);
+			}
+			
+			if (!empty($tmp)) {
+				foreach ($tmp as &$v) {
+					$this->getSubUsersLevel($v['id'], $res, $only_ids);
+				}
+				$res = array_merge($res,$tmp);
+			}
+		}
+		
+		
+		//¬ыборка дочерних ролей
+		public function getSubRoles($pid, $only_ids = false) {
+			$res = array();
+			$this->getSubRolesLevel($pid, $res, $only_ids);
+			return $res;
+		}
+		
+		private function getSubRolesLevel($pid, &$res, $only_ids) {
+			if ($only_ids) {
+				$sql = 'SELECT ar.id FROM auth_role ar
+						LEFT JOIN auth_role ar2 ON ar2.id = ar.pid
+						WHERE ar.pid = '.(int)$pid.'
+						ORDER BY ar.title';
+				$tmp = $this->db->get_all_one($sql);
+			} else {
+				$sql = 'SELECT ar.*, ar2.title creator FROM auth_role ar
+						LEFT JOIN auth_role ar2 ON ar2.id = ar.pid
+						WHERE ar.pid = '.(int)$pid.'
+						ORDER BY ar.title';
+				$tmp = $this->db->get_all($sql);
+			}
+			
+			if (!empty($tmp)) {
+				foreach ($tmp as &$v) {
+					$this->getSubRolesLevel($v['id'], $res, $only_ids);
+				}
+				$res = array_merge($res,$tmp);
+			}
+		}
+		
+		//============================================================
+		
+		//—писок всех страниц (из указанного множества access, если он задан)
+		public function getPages($access = array()) {
+			$sql = 'SELECT id, title 
+					FROM main 
+					WHERE active=1 '.( empty($access) ? '' : 'AND id IN('.join(',',$access).') ' ).
+					'ORDER BY tree';
+			$res = $this->db->get_all($sql);
+			return empty($res) ? array() : $res;
+		}
+		
+		//—писок id страниц, на которые есть доступ
+		public function getAccessPages($role_id) {
+			if ($role_id == 0)	//ƒл€ главного админа
+				$sql = 'SELECT id FROM main ORDER BY tree';
+			else $sql = 'SELECT param1 FROM auth_rights WHERE action_id=2 AND role_id='.$role_id;
+			
+			$res = $this->db->get_all_one($sql);
+			return empty($res) ? array(0) : $res;
+		}
+		
+	}
+?>
